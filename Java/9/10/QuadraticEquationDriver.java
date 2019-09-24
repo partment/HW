2038b884cd5f3d40f3941581079640263fd9911d
PartMent <partment@live.com>
2019-09-24 18:50:32 +0800

    Convert tabs into spaces

--- Java/9/10/QuadraticEquationDriver.java
+++ Java/9/10/QuadraticEquationDriver.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class QuadraticEquationDriver {
-	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in); // Defined a Scanner
-		System.out.println("Given a QuadraticEquation ax^2+bx+c=0");
-		System.out.println("Enter coefficient a");
-		double a = input.nextDouble();
-		System.out.println("Enter coefficient b");
-		double b = input.nextDouble();
-		System.out.println("Enter coefficient c");
-		double c = input.nextDouble();
-		QuadraticEquation equation = new QuadraticEquation(a, b, c); // Construct with a, b and c
-		System.out.println("The Discriminant is "+equation.getDiscriminant());
-		System.out.println("Root1 is "+equation.getRoot1());
-		System.out.println("Root2 is "+equation.getRoot2());
-	}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in); // Defined a Scanner
+        System.out.println("Given a QuadraticEquation ax^2+bx+c=0");
+        System.out.println("Enter coefficient a");
+        double a = input.nextDouble();
+        System.out.println("Enter coefficient b");
+        double b = input.nextDouble();
+        System.out.println("Enter coefficient c");
+        double c = input.nextDouble();
+        QuadraticEquation equation = new QuadraticEquation(a, b, c); // Construct with a, b and c
+        System.out.println("The Discriminant is "+equation.getDiscriminant());
+        System.out.println("Root1 is "+equation.getRoot1());
+        System.out.println("Root2 is "+equation.getRoot2());
+    }
 }
--- Java/9/7/AccountDriver.java
+++ Java/9/7/AccountDriver.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class AccountDriver {
-	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in); // Defined a Scanner
-		Account account = new Account(1122, 20000); // Construct Account with specified id and balance
-		account.setAnnualInterestRate(0.045); // Set AnnualInterestRate to 4.5%
-		System.out.println("Enter an amount you want to withdraw.");
-		account.withdraw(input.nextDouble());
-		System.out.println("Enter an amount you want to deposit.");
-		account.deposit(input.nextDouble());
-		System.out.println("Balance: "+account.getBalance());
-		System.out.println("Monthly Interest: "+account.getMonthlyInterest());
-		System.out.println("Date Created: "+account.getDate());
-	}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in); // Defined a Scanner
+        Account account = new Account(1122, 20000); // Construct Account with specified id and balance
+        account.setAnnualInterestRate(0.045); // Set AnnualInterestRate to 4.5%
+        System.out.println("Enter an amount you want to withdraw.");
+        account.withdraw(input.nextDouble());
+        System.out.println("Enter an amount you want to deposit.");
+        account.deposit(input.nextDouble());
+        System.out.println("Balance: "+account.getBalance());
+        System.out.println("Monthly Interest: "+account.getMonthlyInterest());
+        System.out.println("Date Created: "+account.getDate());
+    }
 }

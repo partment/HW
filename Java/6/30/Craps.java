6d86c3c27455fb2fc6744e7fa575f45b14d98874
PartMent <partment@live.com>
2019-10-07 23:50:38 +0800

    Convert tabs into spaces

--- Java/6/30/Craps.java
+++ Java/6/30/Craps.java
@@ -2,36 +2,36 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 
 public class Craps {
-	private int dice1, dice2;
-	private SecureRandom random = new SecureRandom();
-	public Craps() {}
-	public void Roll() {
-		dice1 = random.nextInt(6) + 1;
-		dice2 = random.nextInt(6) + 1;
-	}
-	public int Sum() {
-		return dice1 + dice2;
-	}
-	public static void main(String args[]) {
-		int end = 0;
-		final Integer craps[] = {2, 3, 12};
-		final Integer natural[] = {7, 11};
-		Craps result = new Craps();
-		while(end == 0) {
-			result.Roll();
-			int sum = result.Sum();
-			if(Arrays.asList(craps).contains(sum)) {
-				System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
-				System.out.println("You lose");
-				end = 1;
-			}else if(Arrays.asList(natural).contains(sum)) {
-				System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
-				System.out.println("You win");
-				end = 1;
-			}else {
-				System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
-				System.out.println("Point is "+result.Sum());
-			}
-		}
-	}
+    private int dice1, dice2;
+    private SecureRandom random = new SecureRandom();
+    public Craps() {}
+    public void Roll() {
+        dice1 = random.nextInt(6) + 1;
+        dice2 = random.nextInt(6) + 1;
+    }
+    public int Sum() {
+        return dice1 + dice2;
+    }
+    public static void main(String args[]) {
+        int end = 0;
+        final Integer craps[] = {2, 3, 12};
+        final Integer natural[] = {7, 11};
+        Craps result = new Craps();
+        while(end == 0) {
+            result.Roll();
+            int sum = result.Sum();
+            if(Arrays.asList(craps).contains(sum)) {
+                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
+                System.out.println("You lose");
+                end = 1;
+            }else if(Arrays.asList(natural).contains(sum)) {
+                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
+                System.out.println("You win");
+                end = 1;
+            }else {
+                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+result.Sum());
+                System.out.println("Point is "+result.Sum());
+            }
+        }
+    }
 }

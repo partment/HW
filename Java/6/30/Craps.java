a3d4b0020505855337336e30bd629c3b12854c20
PartMent <partment@live.com>
2019-10-14 21:32:40 +0800

    Rewrite Craps

--- Java/6/30/Craps.java
+++ Java/6/30/Craps.java
@@ -5,33 +5,36 @@ public class Craps {
     private int dice1, dice2;
     private SecureRandom random = new SecureRandom();
     public Craps() {}
-    public void Roll() {
+    private void Roll() {
         dice1 = random.nextInt(6) + 1;
         dice2 = random.nextInt(6) + 1;
     }
-    public int Sum() {
+    private int Sum() {
         return dice1 + dice2;
     }
-    public static void main(String args[]) {
-        boolean end = false;
+    public void getResult() {
+    	boolean end = false;
         final Integer craps[] = {2, 3, 12};
         final Integer natural[] = {7, 11};
-        Craps result = new Craps();
         while(!end) {
-            result.Roll();
-            int sum = result.Sum();
+            Roll();
+            int sum = Sum();
             if(Arrays.asList(craps).contains(sum)) {
-                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+sum);
+                System.out.println("You rolled "+dice1+" + "+dice2+" = "+sum);
                 System.out.println("You lose");
                 end = true;
             }else if(Arrays.asList(natural).contains(sum)) {
-                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+sum);
+                System.out.println("You rolled "+dice1+" + "+dice2+" = "+sum);
                 System.out.println("You win");
                 end = true;
             }else {
-                System.out.println("You rolled "+result.dice1+" + "+result.dice2+" = "+sum);
+                System.out.println("You rolled "+dice1+" + "+dice2+" = "+sum);
                 System.out.println("Point is "+sum);
             }
         }
+    }
+    public static void main(String args[]) {
+        Craps craps = new Craps();
+        craps.getResult();
     }
 }

0b530369ef02cc3a0e0f7f6ee9ccb93ea4e1b743
PartMent <partment@live.com>
2019-10-20 23:51:56 +0800

    Convert Tabs into Spaces

--- Java/Card/CardDriver.java
+++ Java/Card/CardDriver.java
@@ -1,15 +1,15 @@
 public class CardDriver {
-	public static void main(String args[]) {
-		CardUtils myCard = new CardUtils();
-		myCard.CreateCardSet();
-		myCard.ShuffleCards();
-		Integer cards[] = new Integer[4];
-		System.out.println("---------------------");
-		for(int count = 0;count < 4;count++) {
-			cards[count] = myCard.dealCard();
-			System.out.println(myCard.getCard(cards[count]));
-		}
-		System.out.println("---------------------");
-		System.out.println(myCard.getBigTwo(cards));
-	}
+    public static void main(String args[]) {
+        CardUtils myCard = new CardUtils();
+        myCard.CreateCardSet();
+        myCard.ShuffleCards();
+        Integer cards[] = new Integer[4];
+        System.out.println("---------------------");
+        for(int count = 0;count < 4;count++) {
+            cards[count] = myCard.dealCard();
+            System.out.println(myCard.getCard(cards[count]));
+        }
+        System.out.println("---------------------");
+        System.out.println(myCard.getBigTwo(cards));
+    }
 }

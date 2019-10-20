c7cb2c1fef41bae8f32aeee98d79efb6581ef511
PartMent <partment@live.com>
2019-10-21 00:08:55 +0800

    Convert Tabs into Spaces

--- Java/Card/Card.java
+++ Java/Card/Card.java
@@ -1,26 +1,26 @@
 public class Card {
-	private final int face;
-	private final int suit;
-	private int dealed = 0;
+    private final int face;
+    private final int suit;
+    private int dealed = 0;
 
-	public Card(int faceInput, int suitInput) {
-		this.face = faceInput;
-		this.suit = suitInput;
-	}
+    public Card(int faceInput, int suitInput) {
+        this.face = faceInput;
+        this.suit = suitInput;
+    }
 
-	public void DealThisCard() {
-		this.dealed = 1;
-	}
+    public void DealThisCard() {
+        this.dealed = 1;
+    }
 
-	public int getFace() {
-		return this.face;
-	}
+    public int getFace() {
+        return this.face;
+    }
 
-	public int getSuit() {
-		return this.suit;
-	}
+    public int getSuit() {
+        return this.suit;
+    }
 
-	public boolean getSituation() {
-		return (this.dealed == 1) ? true : false;
-	}
+    public boolean getSituation() {
+        return (this.dealed == 1) ? true : false;
+    }
 }

a5c0cdee5e3d57b5d50122b69f7b48cdef091e17
PartMent <partment@users.noreply.github.com>
2019-11-04 23:26:16 +0800

    Update InitializationDemo

--- Java/InitializationDemo.java
+++ Java/InitializationDemo.java
@@ -1,42 +1,42 @@
 public class InitializationDemo {
-	public static void main(String args[]) {
-		new InitializationDemo();
-		System.out.println("1");
-	}
+    public static void main(String args[]) {
+        new InitializationDemo();
+        System.out.println("1");
+    }
 
-	public InitializationDemo() {
-		new M();
-	}
+    public InitializationDemo() {
+        new M();
+    }
 
-	{
-		System.out.println("2");
-	}
-	static {
-		System.out.println("3");
-	}
+    {
+        System.out.println("2");
+    }
+    static {
+        System.out.println("3");
+    }
 }
 class M extends N {
-	M() {
-		System.out.println("4");
-	}
+    M() {
+        System.out.println("4");
+    }
 
-	{
-		System.out.println("5");
-	}
-	static {
-		System.out.println("6");
-	}
+    {
+        System.out.println("5");
+    }
+    static {
+        System.out.println("6");
+    }
 }
 class N {
-	N() {
-		System.out.println("7");
-	}
+    N() {
+        System.out.println("7");
+    }
 
-	{
-		System.out.println("8");
-	}
+    {
+        System.out.println("8");
+    }
 
-	static {
-		System.out.println("9");
-	}
+    static {
+        System.out.println("9");
+    }
 }

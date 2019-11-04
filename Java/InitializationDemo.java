167ea6d870b9301d3fcb8dc9f1e0b4fd8580e366
PartMent <partment@users.noreply.github.com>
2019-11-04 23:16:03 +0800

    Add InitializationDemo

---
+++ Java/InitializationDemo.java
@@ -0,0 +1,42 @@
+public class InitializationDemo {
+	public static void main(String args[]) {
+		new InitializationDemo();
+		System.out.println("1");
+	}
+
+	public InitializationDemo() {
+		new M();
+	}
+
+	{
+		System.out.println("2");
+	}
+	static {
+		System.out.println("3");
+	}
+}
+class M extends N {
+	M() {
+		System.out.println("4");
+	}
+
+	{
+		System.out.println("5");
+	}
+	static {
+		System.out.println("6");
+	}
+}
+class N {
+	N() {
+		System.out.println("7");
+	}
+
+	{
+		System.out.println("8");
+	}
+
+	static {
+		System.out.println("9");
+	}
+}

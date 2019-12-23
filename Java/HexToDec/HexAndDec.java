d12615fe50848735b6b19b3855ac228cd2ca09d1
PartMent <partment@live.com>
2019-12-24 02:48:24 +0800

    Update HexAndDec

--- Java/HexToDec/HexAndDec.java
+++ Java/HexToDec/HexAndDec.java
@@ -5,13 +5,43 @@ public class HexAndDec {
 
     public HexAndDec () {}
 
-    private static Integer HexToDec(String hex) {
-        return Integer.parseInt(hex, 16);
+    private Integer HexToDec(String hex) {
+        int length = hex.length();
+        int base = 1; 
+        int dec = 0; 
+       
+        for(int i = length-1;i>=0;i--) {    
+            if(hex.charAt(i) >= '0' && hex.charAt(i) <= '9') { 
+                dec += (hex.charAt(i) - 48)*base;  
+                base = base * 16; 
+            }else if(hex.charAt(i) >= 'A' && hex.charAt(i) <= 'F') { 
+                dec += (hex.charAt(i) - 55)*base; 
+                base = base*16; 
+            } 
+        } 
+        return dec;
     }
 
-    private static String DecToHex(int dec) {
-        return Integer.toHexString(dec);
+    private String DecToHex(int dec) {
+        String hex = "";
+        int i = 0;
+        if(dec == 0) hex = "0";
+        while(dec!=0) 
+        {    
+            int tmp  = 0; 
+            tmp = dec % 16; 
+            if(tmp < 10) { 
+                hex = (char)(tmp + 48)+hex; 
+                i++; 
+            }else { 
+                hex = (char)(tmp + 55)+hex; 
+                i++; 
+            }
+            dec = dec/16; 
+        }
+        return hex;
     }
+    
     public static void main(String args[]) {
         HexAndDec manipulater = new HexAndDec();
         System.out.println("Hexadecimal to Decimal press 1");

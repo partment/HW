3ed2855f4108934e893b369e2d9315f53300e22d
PartMent <partment@live.com>
2021-04-29 03:38:30 +0800

    Optimize Banker

--- C/Banker.c
+++ C/Banker.c
@@ -1,4 +1,5 @@
 #define PROC_MAX_COUNT 5
+#define RES_COUNT 3
 #include <stdio.h>
 
 void permute(int *arr, int *possibilities_ptr, int st, int ls, int *counter_ptr);
@@ -19,78 +20,74 @@ int main() {
     int factorial_num = factorial(process_count); //利用階乘求順序的總數
 
     //設定各類資源的數量
-    int instances[3];
-    for (int i = 0; i < 3; i++) {
+    int instances[RES_COUNT];
+    for (int i = 0; i < RES_COUNT; i++) {
         printf("請輸入%c類資源的instance數量：", 65 + i);
         scanf("%d", &instances[i]);
     }
 
     //設定各Process的最大需求數量
-    int max[PROC_MAX_COUNT][3];
+    int max[PROC_MAX_COUNT][RES_COUNT];
     for (int j = 0; j < process_count; j++) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < RES_COUNT; k++) {
             printf("請輸入%d號Process對%c類資源的最大需求數量：", j + 1, 65 + k);
             scanf("%d", &max[j][k]);
         }
     }
 
     //設定各Process已佔用的資源數量
-    int alloc[PROC_MAX_COUNT][3];
+    int alloc[PROC_MAX_COUNT][RES_COUNT];
     for (int j = 0; j < process_count; j++) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < RES_COUNT; k++) {
             printf("請輸入系統快照時%d號Process對%c類資源的已佔用量：", j + 1, 65 + k);
             scanf("%d", &alloc[j][k]);
         }
     }
 
     //算快照時的資源可用數
-    int available[3];
-    for (int i = 0; i < 3; i++) {
+    int available[RES_COUNT];
+    for (int i = 0; i < RES_COUNT; i++) {
         available[i] = instances[i]; //將instances之值複製至available
     }
     for (int j = 0; j < process_count; j++) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < RES_COUNT; k++) {
             available[k] -= alloc[j][k]; //將instances減去已佔用的資源得到available
         }
     }
 
     //算出需求矩陣
-    int need[PROC_MAX_COUNT][3];
+    int need[PROC_MAX_COUNT][RES_COUNT];
     for (int j = 0; j < process_count; j++) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < RES_COUNT; k++) {
             need[j][k] = max[j][k] - alloc[j][k]; //需求 = 最大 - 已佔用
         }
     }
 
     //判斷系統是否安全
     int finish[PROC_MAX_COUNT];
-    int visited[PROC_MAX_COUNT];
     for(int i = 0;i < process_count;i++) {
-        visited[i] = 0;
         finish[i] = 0; //Initialize
     }
     for(int j = 0;j < process_count;j++) {
         if(finish[j] == 0) { //如果沒做完才繼續判斷
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < RES_COUNT; k++) {
                 if(need[j][k] > available[k]) { //如果需求大於可用
                     finish[j] = 0;
                     break;
                 }else { //如果需求小於等於可用
                     finish[j] = 1;
                 }
-                if(finish[j] == 1 && visited[j] == 0) {
-                    visited[j] = 1;
-                    for (int i = 0; i < 3; i++) {
+                if(finish[j] == 1 && k == RES_COUNT - 1) {
+                    for (int i = 0; i < RES_COUNT; i++) {
                         available[i] += alloc[j][i]; //釋放資源
                     }
                 }
-                if(k == 2) {
+                if(k == RES_COUNT - 1) {
                     j = -1; //從頭開始
                 }
             }
         }
     }
-
     //全部finish = 1才算安全
     int safe = 0;
     for(int i = 0;i < process_count;i++) {
@@ -133,11 +130,11 @@ int main() {
         for(int i = 0;i < process_count;i++) {
             finish[i] = 0;
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < RES_COUNT; i++) {
             available[i] = instances[i]; //將instances之值複製至available
         }
         for (int p = 0; p < process_count; p++) {
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < RES_COUNT; k++) {
                 available[k] -= alloc[p][k]; //將instances減去已佔用的資源得到available
             }
         }
@@ -145,7 +142,7 @@ int main() {
         //Start to evaluate this sequence is safe or not  
         for(int k = 0;k < process_count;k++) {
             if(finish[possibilities[j][k]] == 0) { //j = one of permutations, k = one of process in this permutation
-                for (int p = 0; p < 3; p++) {
+                for (int p = 0; p < RES_COUNT; p++) {
                     if(need[possibilities[j][k]][p] > available[p]) { //如果需求大於可用
                         finish[possibilities[j][k]] = 0;
                         break;
@@ -155,7 +152,7 @@ int main() {
                 }
             }
             if(finish[possibilities[j][k]] == 1) {
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < RES_COUNT; i++) {
                     available[i] += alloc[possibilities[j][k]][i]; //釋放資源
                 }
             }
